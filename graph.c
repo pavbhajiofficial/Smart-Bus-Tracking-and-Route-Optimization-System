594392e99521e6a618951bb3f96b3dfe7134258b
pavbhajiofficial <kurapatibhavyasri@gmail.com>
2026-04-07 12:15:11 +0530

    Update graph.c

--- graph.c
+++ graph.c
@@ -1,27 +1,26 @@
 #include <stdio.h>
 #include "graph.h"
 
-// Node names
 char *nodes[N] = {
     "Main Gate", "Anna Audi", "MGR Block", "SMV",
     "TT", "PRP", "SJT", "MGB", "Library"
 };
 
-// Adjacency matrix (distance graph)
+//arbitrary values for this example
 int graph[N][N] = {
-    {0, 6, 4, 0, 0, 0, 0, 0, 7},
-    {6, 0, 3, 0, 0, 0, 0, 0, 5},
-    {4, 3, 0, 5, 0, 0, 0, 0, 0},
-    {0, 0, 5, 0, 6, 0, 0, 0, 0},
-    {0, 0, 0, 6, 0, 4, 3, 0, 0},
-    {0, 0, 0, 0, 4, 0, 2, 5, 0},
-    {0, 0, 0, 0, 3, 2, 0, 4, 0},
-    {0, 0, 0, 0, 0, 5, 4, 0, 6},
-    {7, 5, 0, 0, 0, 0, 0, 6, 0}
+    {0,6,4,0,0,0,0,0,7},
+    {6,0,3,0,0,0,0,0,5},
+    {4,3,0,5,0,0,0,0,0},
+    {0,0,5,0,6,0,0,0,0},
+    {0,0,0,6,0,4,3,0,0},
+    {0,0,0,0,4,0,2,5,0},
+    {0,0,0,0,3,2,0,4,0},
+    {0,0,0,0,0,5,4,0,6},
+    {7,5,0,0,0,0,0,6,0}
 };
 
 void displayStops() {
-    printf("\n📍 Bus Stops:\n");
+    printf("\nBus Stops:\n");
     for (int i = 0; i < N; i++) {
         printf("%d. %s\n", i, nodes[i]);
     }
