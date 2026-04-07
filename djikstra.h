964477f0c7236fcbe41f6410424e4c2e0a5558c1
pavbhajiofficial <kurapatibhavyasri@gmail.com>
2026-04-07 12:11:38 +0530

    Update and rename djikstra.h to processing.h

--- djikstra.h
+++
@@ -1,7 +0,0 @@
-#ifndef DIJKSTRA_H
-#define DIJKSTRA_H
-
-void dijkstra(int src, int dest);
-void findNearest(int src);
-
-#endif
---
+++ processing.h
@@ -0,0 +1,8 @@
+#ifndef PROCESSING_H
+#define PROCESSING_H
+
+void dijkstra(int src, int dest);
+void bfsShortestStops(int src, int dest);
+void findNearest(int src);
+
+#endif
